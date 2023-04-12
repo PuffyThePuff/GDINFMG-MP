4031b6af94a95d1b729395dc2bbf91f2a1c19d85
Youheyo <80012381+Youheyo@users.noreply.github.com>
2023-04-12 23:25:09 +0800

    Placeholder for moves

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@
 const mysql = require("mysql2");
 const express = require("express");
 const { CharacterData } = require("./character");
+const { movesData } = require("./moves");
 
 require("dotenv").config();
 
@@ -52,26 +53,34 @@ pool.query("SELECT * FROM characters;",  function (err, results, fields){
     });
 
     
-    function insertCharacterToDB(character){
+    function InsertCharacterToDB(character){
         ///Works but has an extra `` on strings e.g `Ramlethal` instead of Ramlethal
         ///There could be a more efficient way
-        pool.query(`INSERT IGNORE INTO characters VALUES (??,?, ?, ?, ??, ??, ? , ??, ?, ?);`, [character.name, character.defense, character.guts, character.prejump, character.weight, character.backdash, character.forward_dash, character.unique_movement,character.RISC_multiplier, character.movement_tension_gain],    function (err, result) {
+        pool.query(`INSERT IGNORE INTO characters VALUES (??,?, ?, ?, ??, ??, ? , ??, ?, ?);`, [character.name, character.defense, character.guts, character.prejump, character.weight, character.backdash, character.forward_dash, character.unique_movement,character.RISC_multiplier, character.movement_tension_gain], function (err, result) {
             if (err) throw err;
             console.log(character.name, " has been placed to the Database");
         });
         };
         
-        CharacterData.name = 'Ramlethal';
-        CharacterData.defense = 100;
-        CharacterData.guts = 2;
-        CharacterData.prejump = 2;
-        CharacterData.weight = "Light";
-        CharacterData.backdash = 10;
-        CharacterData.forward_dash = 10;
-        CharacterData.unique_movement = null;
-        CharacterData.RISC_multiplier = 1;
-        CharacterData.movement_tension_gain = 1;
-        insertCharacterToDB(CharacterData);
+        ///Sample Character Data, is not actual data from the game
+        // CharacterData.name = 'Ramlethal';
+        // CharacterData.defense = 100;
+        // CharacterData.guts = 2;
+        // CharacterData.prejump = 2;
+        // CharacterData.weight = "Light";
+        // CharacterData.backdash = 10;
+        // CharacterData.forward_dash = 10;
+        // CharacterData.unique_movement = null;
+        // CharacterData.RISC_multiplier = 1;
+        // CharacterData.movement_tension_gain = 1;
+        // InsertCharacterToDB(CharacterData);
+
+function InsertMovesToDB(move){
+    pool.query('INSERT IGNORE INTO moves VALUES(??, ??, ?, ??, ?, ??, ?, ?, ??, ?, ??, ??, ?, ??, ??)', [move.type, move.moveID, move.damage, move.guard, move.startup, move.active, move.recovery, move.on_block, move.on_hit, move.level, move.counter_type, move.invuln, move.proration, move.risc_gain, move.risc.loss], function (err, result) {
+        if (err) throw err;
+        console.log(move.moveID, " has been placed to the Database");
+    }
+)};
 
 app.get("/", (req, res) => {
     res.send("test get");
