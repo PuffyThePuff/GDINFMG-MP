0a4223f0cc4572a587aef0e1ad6e86a063a99f4c
Nathan-Macabanti <73865062+Nathan-Macabanti@users.noreply.github.com>
2023-04-14 18:34:58 +0800

    table creator

--- MO2/Assets/Scripts/TableCreator.cs
+++ MO2/Assets/Scripts/TableCreator.cs
@@ -11,72 +11,93 @@ public class TableCreator : MonoBehaviour
     [SerializeField] private UseWhich use;
 
     private List<Row> rowList = new();
+    private bool isRunning = false;
 
     public void StartCreateTable(int num)
     {
-        if(use == UseWhich.Async)
-            CreateTable(num);
-        else if(use == UseWhich.Coroutine)
+        if (isRunning) return;
+
+        if (use == UseWhich.Coroutine)
             StartCoroutine(CreateTableCoroutine(num));
-    }
-
-    public async void CreateTable(int num)
-    {
-        foreach(var row in rowList)
-        {
-            Destroy(row);
-        }
-        rowList.Clear();
-        var m_Transform = transform;
-        for (int i = 0; i < num; i++)
-        {
-            var row = Instantiate(rowPrefab, m_Transform);
-            //row.GetComponentInChildren<TextMeshProUGUI>().SetText("uwu");
-            //Set text
-            int length = row.RowLength;
-            for(int j = 0; j < length; j++)
-            {
-                row.SetColumnText(j, "UWU");
-                //yield return null;
-                await Task.Yield();
-            }
-            //Add to list
-            rowList.Add(row);
-            await Task.Yield();
-            //yield return null;
-        }
-
-        await Task.Yield();
-        //yield return null;
+        else if (use == UseWhich.Async)
+            CreateTableAsync(num);
+            
     }
 
     public IEnumerator CreateTableCoroutine(int num)
     {
-        foreach (var row in rowList)
+        isRunning = true;
+        int count = rowList.Count;
+        for(int i = 0; i < count; i++)
         {
-            Destroy(row);
+            Row row = rowList[i];
+            Debug.Log($"Destroying {row.name}");
+            Destroy(row.gameObject);
+            yield return null;
         }
+
         rowList.Clear();
         var m_Transform = transform;
         for (int i = 0; i < num; i++)
         {
             var row = Instantiate(rowPrefab, m_Transform);
+            rowList.Add(row);
+            Debug.Log($"Creating {row.name}");
             //row.GetComponentInChildren<TextMeshProUGUI>().SetText("uwu");
             //Set text
             int length = row.RowLength;
+            //yield return null;
             for (int j = 0; j < length; j++)
             {
                 row.SetColumnText(j, "UWU");
                 yield return null;
                 //await Task.Yield();
             }
-            //Add to list
-            rowList.Add(row);
             //await Task.Yield();
             yield return null;
         }
 
         //await Task.Yield();
         yield return null;
+        isRunning = false;
+    }
+
+    public async void CreateTableAsync(int num)
+    {
+        isRunning = true;
+        int count = rowList.Count;
+        for (int i = 0; i < count; i++)
+        {
+            Row row = rowList[i];
+            Debug.Log($"Destroying {row.name}");
+            Destroy(row.gameObject);
+            await Task.Yield();
+            //yield return null;
+        }
+
+        rowList.Clear();
+        var m_Transform = transform;
+        for (int i = 0; i < num; i++)
+        {
+            var row = Instantiate(rowPrefab, m_Transform);
+            rowList.Add(row);
+            Debug.Log($"Creating {row.name}");
+            //row.GetComponentInChildren<TextMeshProUGUI>().SetText("uwu");
+            //Set text
+            int length = row.RowLength;
+            //yield return null;
+            for (int j = 0; j < length; j++)
+            {
+                row.SetColumnText(j, "UWU");
+                //yield return null;
+                await Task.Yield();
+            }
+            await Task.Yield();
+            //yield return null;
+        }
+
+        await Task.Yield();
+        //yield return null;
+        isRunning = false;
     }
 }
